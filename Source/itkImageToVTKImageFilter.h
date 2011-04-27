2bde1bd3d4a69c3d32d89ef0c7f32c640d592498
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-04-27 15:49:00 -0400

    add copyright

--- Source/itkImageToVTKImageFilter.h
+++ Source/itkImageToVTKImageFilter.h
@@ -1,19 +1,20 @@
 /*=========================================================================
-
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkImageToVTKImageFilter.h,v $
-  Language:  C++
-  Date:      $Date: 2007-11-20 12:46:10 -0500 (Tue, 20 Nov 2007) $
-  Version:   $Revision: 477 $
-
-  Copyright (c) 2002 Insight Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 
 #ifndef __itkImageToVTKImageFilter_h
 #define __itkImageToVTKImageFilter_h
